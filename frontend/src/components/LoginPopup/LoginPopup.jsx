7b8100f7d66a302b2f0755cbfad5421e704ecfd5
sumitpadma <sumitpadma@gmail.com>
2025-01-22 19:58:06 +0530

    signinup

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./LoginPopup.css";
 import { assets } from "../../assets/assets";
 
 const LoginPopup = ({ setShowLogin }) => {
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import {StoreContext} from "../../context/StoreContext";
 const Cart = () => {
-  return <div>Cart</div>;
+
+  const {CartItems,food_list,removeFromCart} = useContext(StoreContext);
+
+  return <div className="cart">
+    <div className="cart-items">
+      <div className="cart-items-title">
+        <p>Items</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <br/>
+      <hr/>
+    </div>
+  </div>;
 };
 
 export default Cart;
