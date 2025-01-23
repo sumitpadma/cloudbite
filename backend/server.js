2aa8ad70fc007c0cd160d89250a4d8220cabbb48
sumitpadma <sumitpadma@gmail.com>
2025-01-23 13:16:17 +0530

    backend-setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express, { response } from "express";
+import cors from "cors";
+
+//app config
+const app = express();
+const port = 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+
+app.listen(port, () => {
+  console.log("Server started at http://localhost:" + port);
+});
